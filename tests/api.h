6621acd3c2c37232d5528b25f855091e37ee25fa
Dave Bort <git@dbort.com>
2025-09-14 11:16:47 -0700

    Simplifying API

--- tests/api.h
+++ tests/api.h
@@ -3,59 +3,40 @@
 #include <inttypes.h>
 #include <assert.h>
 
+//
+// Global
+//
+
+//
+// Portable API
+//
+
+// generic, uses uhal types
+
+typedef struct {
+  gpio_pin_t pin;
+} board_signal_info_t;
+
+typedef struct {
+  i2c_t bus;
+  uint8_t addr;
+} board_i2c_peripheral_info_t;
+
+// BSP interface only; internals not exposed to consumer
+typedef struct {
+  // Table that maps board_signal_t to info entries.
+  board_signal_info_t* signal_map;
+  size_t signal_map_size;
+  // Table that maps board_i2c_peripheral_t to info entries.
+  board_i2c_peripheral_info_t* i2c_peripheral_map;
+  size_t i2c_peripheral_map_size;
+} board_mapping_t;
+
+//
 // Fixed for chip
+//
 
-// 3 bits?
-typedef enum {
-    BUS_TYPE_I2C,
-    BUS_TYPE_I3C,
-    BUS_TYPE_SPI,
-    BUS_TYPE_1W,
-    BUS_TYPE_USB,
-    BUS_TYPE_MAX,
-} bus_type_t;
-
-#define DECLARE_BUS(type, index) ((type)<<4 | (index))
-#define GET_BUS_TYPE(bus) ((bus)>>4)
-#define GET_BUS_INDEX(bus) ((bus)&0xf)
-
-typedef enum {
-    BUS_I2C0 = DECLARE_BUS(BUS_TYPE_I2C, 0),
-    BUS_I2C1 = DECLARE_BUS(BUS_TYPE_I2C, 1),
-    BUS_I2C2 = DECLARE_BUS(BUS_TYPE_I2C, 2),
-    BUS_SPI0 = DECLARE_BUS(BUS_TYPE_SPI, 0),
-    BUS_MAX,
-} bsp_bus_t;
-
-// Fixed for product across board IDs, though not all may be mapped
-// for a given ID.
-
-// Common namespace of all signals, GPIOs or otherwise.
-//? What about GPIO extenders?
-typedef enum {
-    SIGNAL_AMP_L_RESET, // Skip the _N since ACTIVE_LOW takes care of it
-    SIGNAL_AMP_R_RESET, // Skip the _N since ACTIVE_LOW takes care of it
-    SIGNAL_DEBUG_UART_TX,
-    SIGNAL_DEBUG_UART_RX,
-    SIGNAL_BUTTON_N,
-    SIGNAL_POWER,
-    SIGNAL_MOTOR_SS, // Skip the _N since ACTIVE_LOW takes care of it
-    SIGNAL_MAX,
-    SIGNAL_UNKNOWN,
-} bsp_signal_t;
-
-// Set of off-chip peripherals. Could specialize to a given bus
-// type, since we're not going to transparently move a peripheral
-// from i2c to SPI, but it's also ok to mix all names here and
-// leave that to the API level.
-typedef enum {
-    PERIPHERAL_AUDIO_AMP_L,
-    PERIPHERAL_AUDIO_AMP_R,
-    PERIPHERAL_MOTOR,
-    PERIPHERAL_MAX,
-} bsp_peripheral_t;
-
-// Depends on board ID
+// uhal
 
 typedef enum {
   GPIO_0,
@@ -63,114 +44,137 @@ typedef enum {
   GPIO_2,
 } gpio_pin_t;
 
-typedef struct {
-    gpio_pin_t pin;
-    uint32_t flags; // in vs out vs special; pull up/down/none; 
-} bsp_signal_desc_t;
+// The type used by the drivers, so that users don't need to convert
+typedef enum {
+  BUS_I2C0,
+  BUS_I2C1,
+  BUS_I2C2,
+  BUS_MAX,
+} i2c_t;
 
-// include/dt-bindings/gpio/gpio.h
-// https://elinux.org/images/a/a7/ELC-2021_Introduction_to_pin_muxing_and_GPIO_control_under_Linux.pdf
+//
+// Fixed for product across board IDs, though not all may be mapped
+// for a given ID.
+//
 
-/* Bit 0 express polarity */
-#define GPIO_ACTIVE_HIGH (0<<0)
-#define GPIO_ACTIVE_LOW (1<<0)
-/* Bit 4 express pull up */
-#define GPIO_PULL_UP (1<<4) 
-/* Bit 5 express pull down */
-#define GPIO_PULL_DOWN (1<<5) 
+// Common namespace of all signals connected directly to chip pins.
+//? What about GPIO extenders?
+typedef enum {
+    SIGNAL_AMP_L_RESET_N,
+    SIGNAL_AMP_R_RESET_N,
+    SIGNAL_DEBUG_UART_TX,
+    SIGNAL_DEBUG_UART_RX,
+    SIGNAL_BUTTON_N,
+    SIGNAL_POWER,
+    SIGNAL_MAX,
+    SIGNAL_UNKNOWN,
+} board_signal_t;
 
-// Users know the semantics of the pin, whether it's an input or output
+// Set of I2C peripherals.
+typedef enum {
+    PERIPHERAL_AUDIO_AMP_L,
+    PERIPHERAL_AUDIO_AMP_R,
+    PERIPHERAL_MAX,
+} board_i2c_peripheral_t;
+
+// Depends on board ID
+
+// Users know the semantics of the pin, whether it's an input or output, pulled up or not
 // We'd only need to know it here if we wanted to auto-configure all pins,
 // but the current pattern is to let the driver config the pins
-static bsp_signal_desc_t dev0_signals[SIGNAL_MAX] = {
-    [SIGNAL_AMP_L_RESET] = { .pin = GPIO_0, .flags = GPIO_ACTIVE_LOW | GPIO_PULL_UP },
-    [SIGNAL_AMP_R_RESET] = { .pin = GPIO_1, .flags = GPIO_ACTIVE_LOW | GPIO_PULL_UP },
-    [SIGNAL_POWER] = { .pin = GPIO_2, .flags = GPIO_ACTIVE_HIGH | GPIO_PULL_UP },
+static board_signal_info_t dev0_signals[SIGNAL_MAX] = {
+    [SIGNAL_AMP_L_RESET_N] = { .pin = GPIO_0 },
+    [SIGNAL_AMP_R_RESET_N] = { .pin = GPIO_1 },
+    [SIGNAL_DEBUG_UART_RX] = { .pin = GPIO_2 },
 };
 
-typedef struct {
-    bsp_bus_t bus;
-    union {
-      uint8_t i2c_addr;
-      bsp_signal_t spi_ss_signal;
-    };
-} bsp_peripheral_desc_t;
-
-// why mix buses? callers will know which type of bus a thing is on
-static bsp_peripheral_desc_t dev0_peripherals[PERIPHERAL_MAX] = {
-    [PERIPHERAL_AUDIO_AMP_L] = { .bus = BUS_I2C0, .i2c_addr = 0x45 },
-    [PERIPHERAL_AUDIO_AMP_R] = { .bus = BUS_I2C2, .i2c_addr = 0x20 },
-    [PERIPHERAL_MOTOR] = { .bus = BUS_SPI0, .spi_ss_signal = SIGNAL_MOTOR_SS },
+static board_i2c_peripheral_info_t dev0_i2c_peripherals[PERIPHERAL_MAX] = {
+    [PERIPHERAL_AUDIO_AMP_L] = { .bus = BUS_I2C0, .addr = 0x45 },
+    [PERIPHERAL_AUDIO_AMP_R] = { .bus = BUS_I2C2, .addr = 0x20 },
 };
 
-typedef struct {
-    bsp_signal_t signal;
-    gpio_pin_t pin;
-    // Three lists? inputs, outputs, special?
-} bsp_signal_mapping_t;
+static board_mapping_t dev0_board_mapping = {
+  .signal_map = dev0_signals,
+  .signal_map_size = SIGNAL_MAX,
+  .i2c_peripheral_map = dev0_i2c_peripherals,
+  .i2c_peripheral_map_size = PERIPHERAL_MAX,
+};
 
-// Usage
+//
+// API impl
+//
 
-typedef void bsp_signal_map_t;
-
-bsp_signal_map_t* bsp_get_signal_map() {
-  // Check boardID
-  return dev0_signals;
+// Internal BSP-defined function
+const board_mapping_t* bsp_get_board_mapping() {
+  return &dev0_board_mapping;
 }
 
-bsp_signal_desc_t* bsp_get_signal(bsp_signal_map_t* pmap, bsp_signal_t signal) {
-  if (pmap == NULL || signal < 0 || signal >= SIGNAL_MAX) {
-    return NULL;
-  }
-  return ((bsp_signal_desc_t*)pmap) + signal;
-}
-
-typedef void bsp_peripheral_map_t;
-
-bsp_peripheral_map_t* bsp_get_peripheral_map() {
-  // Check boardID
-  return dev0_peripherals;
-}
-
-bsp_peripheral_desc_t* bsp_get_peripheral(bsp_peripheral_map_t* pmap, bsp_peripheral_t peripheral) {
-  if (pmap == NULL || peripheral < 0 || peripheral >= PERIPHERAL_MAX) {
-    return NULL;
-  }
-  return ((bsp_peripheral_desc_t*)pmap) + peripheral;
-}
-
-int audio_amp_init() {
-  bsp_peripheral_map_t* pmap = bsp_get_peripheral_map();
-  bsp_peripheral_desc_t* amp_l = bsp_get_peripheral(pmap, PERIPHERAL_AUDIO_AMP_L);
-  bsp_peripheral_desc_t* amp_r = bsp_get_peripheral(pmap, PERIPHERAL_AUDIO_AMP_R);
-  if (amp_l == NULL || amp_r == NULL) {
+int board_get_signal(board_signal_t signal, board_signal_info_t* out_info) {
+  if (out_info == NULL) {
     return -1;
   }
-  // awkward if amp_l->bus isn't compatible with uhal i2c enum
-  // could have a macro to convert, and sneak the uhal enum into the bus enum
-  // or have different types/functions per bus
-
-  bsp_signal_map_t* smap = bsp_get_signal_map();
-  bsp_signal_desc_t* amp_l_reset = bsp_get_signal(smap, SIGNAL_AMP_L_RESET);
-  bsp_signal_desc_t* amp_r_reset = bsp_get_signal(smap, SIGNAL_AMP_R_RESET);
-
-#if 0
-  // if we're using ACTIVE_LOW, ideally some underlying API would translate that for us
-  // Maybe there's a signal config/read/write layer above the gpio layer
-  // or we leave it to the user
-  gpio_out_cfg(amp_l_reset->pin);
-  if (amp_l_reset->flags & GPIO_ACTIVE_LOW) {
-    gpio_write(amp_l_reset->pin, 1);
-  } else {
-    gpio_write(amp_l_reset->pin, 0);
+  // Another option would be for the caller to get and pass in the board mapping,
+  // but that logic would always be the same. It would be more testable to inject
+  // it, but we can easily override this function in testing since it's implemented
+  // by the bsp.
+  const board_mapping_t* map = bsp_get_board_mapping();
+  if (map == NULL || signal < 0 || signal >= map->signal_map_size) {
+    return -1;
   }
-#endif
+
+  // Having the user provide the struct lets the internal mapping use a
+  // different layout if desired.
+  *out_info = map->signal_map[signal];
+  return 0;
 }
 
-// Bus addresses:
-// - Also need a peripheral map with i2c bus and address, since that can
-//   change between board revisions. Same for i3c, SPI, etc. Or even 1wire pins
-// - i2c bus metadata? app or sensor controls a bus? That could be in code to start with
+int board_get_i2c_peripheral(board_i2c_peripheral_t peripheral, board_i2c_peripheral_info_t* out_info) {
+  if (out_info == NULL) {
+    return -1;
+  }
+  const board_mapping_t* map = bsp_get_board_mapping();
+  if (map == NULL || peripheral < 0 || peripheral >= map->i2c_peripheral_map_size) {
+    return -1;
+  }
+
+  // Having the user provide the struct lets the internal mapping use a
+  // different layout if desired, at the expense of a copy.
+  *out_info = map->i2c_peripheral_map[peripheral];
+  return 0;
+}
+
+//
+// User code
+//
+
+int audio_amp_init() {
+  int status;
+  board_i2c_peripheral_info_t amp_l;
+  status = board_get_i2c_peripheral(PERIPHERAL_AUDIO_AMP_L, &amp_l);
+  if (status != 0) {
+    return status;
+  }
+  // Can use amp_l.bus and amp_l.addr to talk to the peripheral.
+
+  board_i2c_peripheral_info_t amp_r;
+  status = board_get_i2c_peripheral(PERIPHERAL_AUDIO_AMP_R, &amp_r);
+  if (status != 0) {
+    return status;
+  }
+
+  board_signal_info_t amp_l_reset_n;
+  status = board_get_signal(SIGNAL_AMP_L_RESET_N, &amp_l_reset_n);
+  if (status != 0) {
+    return status;
+  }
+  // Can use amp_l_reset_n.pin to configure and use the signal.
+
+  board_signal_info_t amp_r_reset_n;
+  status = board_get_signal(SIGNAL_AMP_L_RESET_N, &amp_l_reset_n);
+  if (status != 0) {
+    return status;
+  }
+}
 
 //?? Multiple signals may map to a single pin, especially if there are jumpers
 //   Or if the signal changes at runtime depending on the state (camera vs mic)
@@ -188,22 +192,9 @@ A higher layer could manage enabling, low-power modes etc. This lower layer coul
 metadata to help with that ("boot state", "low power state") but doesn't change anything
 on its own.
 
-- Enable a signal:
-  - Set up its pin. It might override another signal that's currently using the same pin,
-    but that might be up to the higher levels to worry about. Or we could keep a map
-    of whether a given pin is enabled, and fail if we try to re-enable it
-    - "set up its pin": configure it to its initial state
+If we generate these mappings from a spreadsheet, we could generate a different table of
+states to set pins to for major system transitions: boot, sleep. That data doesn't need
+to be visible to users of the board API.
 
-- Put system in a different state: low-power, boot, default, custom
-  - Moving in and out of low-power: do we record the previous state so we can restore it?
-    Or require that all drivers have disabled their signals at that point?
-
-- I'm the i2c driver and someone just told me to enable bus 0
-  - uhal currently requires putting the pins in the init config, so we'd need to
-    get the pins for that bus
-  - could have a custom "i2c pin group" struct, but then would also need custom pin
-    groups for every type of peripheral
-  - start out by having signals that map all pins: I2C0_SDA, I2C0_SCL
-  - i2c driver knows that it should configure those pins as a specific function with
-    appropriate pullup/down, so we don't need that in the pin map
+But it could be stored in the same internal tables, we just wouldn't expose it to users.
 */
